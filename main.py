8b71612abdc4b8b5b591edb8e2d47088b1a007b2
oshitiyob <osheriyov@gmail.com>
2024-09-18 17:02:02 +0300

    car functions

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import time
+
 import pygame
+
+import car_functions
 import screen
 import consts
 
+timer_event = pygame.event.custom_type()
+pygame.time.set_timer(timer_event, 500)
 state = {
     "state": consts.RUNNING_STATE,
     "is_window_open": True,
     "car_position": consts.INITIAL_CAR_POSITION,
     "car_speed": 0,
     "first_line_position": consts.INITIAL_LINE_POSITION,
-    "movement": True
+    "movement": False,
+    "timer": 0
 }
+
 
 def main():
     pygame.init()
@@ -26,28 +34,39 @@ def handle_user():
             state["is_window_open"] = False
 
         elif state["state"] == consts.RUNNING_STATE:
-            if state["movement"]:
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        state["movement"] = False
-                        continue
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pass
 
-                    if event.key == pygame.K_LEFT:
-                        print("left")
+                if event.key == pygame.K_LEFT:
+                    print("left")
 
-                    elif event.key == pygame.K_RIGHT:
-                        print("right")
+                if event.key == pygame.K_RIGHT:
+                    print("right")
 
-                    elif event.key == pygame.K_UP:
-                        print("up")
+                if event.key == pygame.K_UP:
+                    state["movement"] = True
 
-                    elif event.key == pygame.K_DOWN:
-                        print("down")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    state["movement"] = False
+
+                if event.key == pygame.K_DOWN:
+                    state["car_speed"] -= 1
+                    print()
+
+        if state["movement"]:
+            state["car_speed"] += 1
+            print(state["car_speed"])
+
+
 
 def handle_object_position():
-    i = 1
-    if state["first_line_position"] + i >= consts.LINES_ONLY_SPACE:
+    state["car_speed"] = 1
+    if state["first_line_position"] + state["car_speed"] >= consts.LINES_ONLY_SPACE:
         state["first_line_position"] = -consts.LINES_HEIGHT
     else:
-        state["first_line_position"] = state["first_line_position"] + i
+        state["first_line_position"] = state["first_line_position"] + state["car_speed"]
+
+
 main()
