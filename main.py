b88f0cfafdaee23fe3954b2bb68f3d856ff36728
aviad <aviadbenyaish@gmail.com>
2024-09-18 11:34:49 +0300

    UPDATE

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
-
+import screen
 import consts
 
 state = {
     "state": consts.RUNNING_STATE,
-    "is_window_open": False,
+    "is_window_open": True,
+    "car_position": consts.INITIAL_CAR_POSITION
 }
 
 def main():
     pygame.init()
     while state["is_window_open"]:
         hendle_user_events()
+        screen.draw_game(state)
+
 def hendle_user_events():
+    print()
 
 
+main()
--- screen.py
+++ screen.py
@@ -1,16 +1,19 @@
 import pygame
+
+import consts
 from consts import *
 
 screen = pygame.display.set_mode(
     (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
-def draw_game():
-    draw_player()
+def draw_game(state):
+    screen.fill(consts.BACKGROUND_COLOR)
+    draw_player(state["car_position"])
+    pygame.display.flip()
 
 
-def draw_player():
-    pygame.init()
-    car = CAR
-    pygame.image.load(car)
-    pygame.transform.scale(car, [CAR_WIDTH, CAR_HEIGHT])
+def draw_player(car_position):
+    car_image = consts.MAIN_CAR_IMAGE
+    car_image = pygame.transform.scale(car_image, [CAR_WIDTH, CAR_HEIGHT])
+    screen.blit(car_image, car_position)
