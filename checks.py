136dcfc75b0af5abdc927a2a14c6ed597a91e707
Or <orgelb06@gmail.com>
2024-09-18 12:32:26 +0300

    checks

---
+++ checks.py
@@ -0,0 +1,16 @@
+import consts
+import questions
+
+def distance_car_to_sign(car_y, sign_y):
+    if car_y-sign_y <= consts.SCREEN_HEIGHT/4:
+        return True
+    else:
+        return False
+
+def check_answer(question_list, player_answer_int, sign):
+    if questions.answers[sign][(player_answer_int-1)] == question_list[2]:
+        return True
+    else:
+        return False
+
+
