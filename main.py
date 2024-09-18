f81e51a31e41dcbfb1f185e8b881dc0468df0d52
aviad <aviadbenyaish@gmail.com>
2024-09-18 11:16:50 +0300

    UPDATE

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import pygame
 import consts
 
 state = {
-    "state": consts.RUNNING_STATE
+    "state": consts.RUNNING_STATE,
+    "is_window_open": False,
 }
+
+def main():
+    pygame.init()
+    while state["is_window_open"]:
+        hendle_user_events()
+def hendle_user_events():
+
+
