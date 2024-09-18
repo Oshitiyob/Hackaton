e1e674137db4bed9c0beadd0760ee0db069b1dcf
aviad <aviadbenyaish@gmail.com>
2024-09-18 11:42:49 +0300

    UPDATE

--- screen.py
+++ screen.py
@@ -2,7 +2,7 @@ import pygame
 import consts
 
 screen = pygame.display.set_mode(
-    (SCREEN_WIDTH, SCREEN_HEIGHT))
+    (consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
 
 
 def draw_game(state):
@@ -13,5 +13,5 @@ def draw_game(state):
 
 def draw_player(car_position):
     car_image = consts.MAIN_CAR_IMAGE
-    car_image = pygame.transform.scale(car_image, [CAR_WIDTH, CAR_HEIGHT])
+    car_image = pygame.transform.scale(car_image, [consts.CAR_WIDTH, consts.CAR_HEIGHT])
     screen.blit(car_image, car_position)
