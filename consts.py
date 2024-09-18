ad2d79a7ad58cf728ead322379a78b8897ab0d4a
aviad <aviadbenyaish@gmail.com>
2024-09-18 11:05:35 +0300

    UPDATE

---
+++ consts.py
@@ -0,0 +1,4 @@
+import pygame
+MAIN_CAR_IMAGE = pygame.image.load("./images/main_car.png")
+RUNNING_STATE = 1
+QUESTION_STATE = 2
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("wow")
+import pygame
+
+import consts
+
+state = {
+    "state": consts.RUNNING_STATE
+}
