634cb0fb0b54c307863cc2600afd6788f1d89724
oshitiyob <osheriyov@gmail.com>
2024-09-18 11:31:15 +0300

    main changes

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import pygame
-
-import consts
+import screen
+from consts import *
 
 state = {
-    "state": consts.RUNNING_STATE
+    "state": RUNNING_STATE,
+    "is_window_open": True
 }
+
+
+def main():
+    handle_user()
+
+
+def handle_user():
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+        elif state["state"] == RUNNING_STATE:
+            if state["movement"]:
+                if event.key == pygame.K_SPACE:
+                    state["movement"] = False
+                    state["view_mines"] = True
+                    continue
+
+                if event.key == pygame.K_LEFT:
+                    print("left")
+
+                elif event.key == pygame.K_RIGHT:
+                    print("right")
+
+                elif event.key == pygame.K_UP:
+                    print("up")
+
+                elif event.key == pygame.K_DOWN:
+                    print("down")
