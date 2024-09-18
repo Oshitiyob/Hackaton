3e2971a41f34c14d117d2569bba74612b1689c23
oshitiyob <osheriyov@gmail.com>
2024-09-18 19:49:50 +0300

    unfinished acceleration

--- screen.py
+++ screen.py
@@ -30,7 +30,6 @@ def draw_line(line_position_y):
 
 
 def draw_road_lines(first_line_position_y):
-    print(first_line_position_y)
     line_position_y = first_line_position_y
     while line_position_y < consts.SCREEN_HEIGHT:
         draw_line(line_position_y)
