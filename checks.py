2c15176f96ca1d4d1028150eefab7751b3b6089b
oshitiyob <osheriyov@gmail.com>
2024-09-19 10:40:59 +0300

    checks update

--- checks.py
+++ checks.py
@@ -2,10 +2,13 @@ import consts
 
 
 def distance_car_to_sign(car_y, sign_y):
-    if car_y - sign_y <= consts.SCREEN_HEIGHT/2:
+    if car_y - sign_y <= consts.SCREEN_HEIGHT / 2:
         return True
     else:
         return False
 
 
-
+def check_answer(state):
+    if state["question_answer"][0] == consts.ANSWERS[state["sign"]]:
+        return True
+    return False
